6ccdb2af5d3784210df7258f915bed132d9f6e2e
heavenring <anstn0104@naver.com>
2025-04-12 13:55:52 +0900

    add: MainPage download setting

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.main_page, name='home'),  # 루트 URL 요청 -> views.home 실행
     path('login/', views.login_view, name='login'),
+    path('download/', views.download_item, name='download'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
-from .utils import get_sftp_file_list
+from .utils import get_sftp_file_list, download_file
+
+
+def login_check(request):
+
+    return request.session.get('is_logged_in')
 
 def main_page(request):
-    if request.session.get('is_logged_in'):
+    if login_check(request):
         file_list = get_sftp_file_list('.')
         return render(request, 'MainPage.html', {'file_list': file_list})
     else:
-        return render(request, 'MainPage.html')
+        return redirect('login')
 
 @csrf_exempt
 def login_view(request):
@@ -18,4 +23,11 @@ def login_view(request):
         if username == 'admin' and password == '1234':
             request.session['is_logged_in'] = True
         return redirect('home')
-    return redirect('home')
+    return redirect('login')
+
+@csrf_exempt
+def download_item(request):
+    if login_check(request):
+        download_file()
+    else:
+        return redirect('login')
